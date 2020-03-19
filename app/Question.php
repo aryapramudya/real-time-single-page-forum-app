cdbcc910c39b85f80a2f09cdf41b9717d0849c34
Arya Pramudya <aryapramudya.gunadarma@gmail.com>
2020-03-20 04:52:34 +0700

    Relationships

--- app/Question.php
+++ app/Question.php
@@ -3,8 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Reply;
+use App\Category;
 
 class Question extends Model
 {
-    //
+    //get data siapa yang buat question nya dari table question
+    public function user(){
+    	return $this->belongsTo(User);
+    }
+
+    //get data replies yang di komentarin di setiap question dari table question
+    public function replies(){
+    	return $this->hasMany(Reply);
+    }
+
+    //get data jenis category di setiap question dari table question
+    public function category(){
+    	return $this->belongsTo(Category);
+    }
 }
--- app/Reply.php
+++ app/Reply.php
@@ -3,8 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Question;
+use App\User;
+use App\Like;
 
 class Reply extends Model
 {
-    //
-}
+    //get data relasi reply ke question yang mana dari table reply
+    public function question(){
+    	return $this->belongsTo(Question);
+    }
+
+    //get data siapa yang buat reply nya dari table reply
+    public function user(){
+    	return $this->belongsTo(User);
+    }
+
+    //get data daftar like berdasarkan reply nya dari table reply
+    public function like(){
+    	return $this->hasMany(Like);
+    }
+} 
