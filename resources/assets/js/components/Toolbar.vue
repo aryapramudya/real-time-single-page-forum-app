f137aba7b0ba400471cd4e1395b8c8c69ac36e1d
Arya Pramudya <aryapramudya.gunadarma@gmail.com>
2020-03-26 01:25:10 +0700

    Vuetify, Vue Router and Initial Components

---
+++ resources/assets/js/components/Toolbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card
+    color="grey lighten-4"
+    flat
+    height="200px"
+    tile
+  >
+    <v-toolbar dense>
+      <!--<v-app-bar-nav-icon></v-app-bar-nav-icon>-->
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn>Forum</v-btn>
+      <v-btn>Ask Question</v-btn>
+      <v-btn>Category</v-btn>
+      <v-btn>Login</v-btn>
+    </v-toolbar>
+  </v-card>
+</template>
